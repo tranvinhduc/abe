158a7c5ba574b9a3be368514adc937fe24c8b2ea
Tran Vinh Duc <tranvinhduc@gmail.com>
2017-12-07 10:24:04 +0700

    -a

---
+++ readfile.h
@@ -0,0 +1,11 @@
+#ifndef readFile_h
+#define readFile_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "dnf.h"
+
+void readFiles(const char *filename, Set *userAttrs, DNF *B);
+#endif /* readFile_h */
